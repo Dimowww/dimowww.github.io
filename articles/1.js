3772a20d712a16213da9ac9e12c6147089f14ef1
Dimowww <lardimir@mail.ru>
2021-11-14 19:00:19 +0300

    f
    f

---
+++ articles/1.js
@@ -0,0 +1,22 @@
+var articles = [
+    {
+        title: '#',
+        link: '1str.html'
+    }
+]
+
+var blog = '<ul>', article, amountOfArticles = articles.length
+
+for (article = 0; article < amountOfArticles; article++) {
+    blog += `
+    <li> 
+        <a href= "articles/${articles[article].link}" class="article"> 
+            <h5>${articles[article].title}</h5>
+        </a>
+    </li> `
+}
+
+blog += '</ul>'
+
+var root = document.querySelector('#root')
+root.innerHTML = blog
